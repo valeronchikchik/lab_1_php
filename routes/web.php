062e3fb4e6d599be9c12f8943f037543db16be8a
valeronchikchik <tsanko.valerii@student.uzhnu.edu.ua>
2026-03-06 21:52:59 +0200

    lab1 tasks

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+
+Route::resource('users', UserController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
